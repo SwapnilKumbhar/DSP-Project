a0566b10410a59b91174b730c3810f88a26119d6
SwapnilKumbhar <swapsk05@gmail.com>
2017-10-21 21:07:56 +0530

    Alot of work.

--- Auto.py
+++ Auto.py
@@ -7,26 +7,48 @@ class Autotune:
         self.INPUT_WAVE = y
         self.INPUT_SR = sr
         self.SCALE = scale
-        self._note = Notes.Notes()
+        self._note = Notes.Notes(scale)
         
-        self.NOTES = self._note.getScale(scale)
+        self.NOTES = self._note.getScale()
+        self.OUTPUT_WAVE = np.empty(shape=self.INPUT_WAVE.shape)
 
     def correct(self):
-        step = int(sr/2)
-        for x in range(0,len(y),step):
+        step = int(self.INPUT_SR/20)
+        for x in range(0,len(self.INPUT_WAVE),step):
             # find STFT
             # Match to closest frequency
             # transpose
             # Append to self.OUTPUT_WAVE
             # Return output wave
+            
+            y = self.INPUT_WAVE[x:x+step]
+            f = self._findStft(y)
+            
+            diff_array = [ np.abs(note - f) for note in self.NOTES ]
+            note = np.argmin(diff_array)
+            print(f,end='\t')
+            print(self.NOTES[note],end='\t')
 
-        return
+            self.OUTPUT_WAVE[x:x+step] = self._transpose(y, f, self.NOTES[note])
+            #self.OUTPUT_WAVE = np.concatenate(self.OUTPUT_WAVE,self._transpose(y, f, self.NOTES[note]))
+
+        return librosa.util.normalize(self.OUTPUT_WAVE)
     
-    def _findStft(self):
+    def _findStft(self,y):
         # Find stft, use argmax, find mean if req
         # Return max amp frequency in the interval
-        return
+
+        yD = librosa.stft(y,n_fft=self.INPUT_SR)
+        arr = np.argmax(yD,axis=0)
+        fq = np.mean(arr)
+        print(fq)
+
+        return self._note.normalize(fq)
     
-    def _transpose(self, fold, fnew):
+    def _transpose(self, y, fold, fnew):
         # Calculate the steps to be transposed based on the old and new frequencies.
-        return
+        steps = self._note.getStep(fold,fnew)
+        print(steps)
+        yT = librosa.effects.pitch_shift(y,self.INPUT_SR,steps)
+        return yT
+
--- Notes.py
+++ Notes.py
@@ -5,17 +5,20 @@ class Notes:
     NOTES = []
     MAJOR = [0,2,4,5,7,9,11]
 
-    def __init__(self):
+    def __init__(self,scale):
+        self.TONE = self.TONE * 2**(scale/12)
         for x in range(0,12):
             self.NOTES.append(self.TONE * 2**(x/12))
 
-    def getScale(self,Trans):
+    def getScale(self):
         SCALE = []
         for num in self.MAJOR:
-            SCALE.append(self.NOTES[(Trans+num)%12])
+            SCALE.append(self.NOTES[num%12])
         return SCALE
 
     def normalize(self,note):
+        if int(note) == 0:
+            return note
         if note < self.TONE:
             return self.normalize(note*2)
         elif note > 2*self.TONE:
@@ -23,5 +26,7 @@ class Notes:
         else:
             return note
 
-    def getStep(self,fnew,fold):
+    def getStep(self,fold,fnew):
+        if int(fold) == 0:
+            return fold
         return 12*math.log(fnew/fold,2)
